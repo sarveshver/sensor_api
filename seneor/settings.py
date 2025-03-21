c95324c706280c935bb45136c5a9eca281563ee7
sarveshver <sarveshver77@gmail.com>
2025-03-21 22:09:19 +0530

    Updated ALLOWED_HOSTS for Render

--- seneor/settings.py
+++ seneor/settings.py
@@ -10,8 +10,10 @@ STATICFILES_DIRS = [
 import os  
 SECRET_KEY = os.getenv('SECRET_KEY', 'a91eff586dcf63855387764b8b8bde25')
 
-DEBUG = True
-ALLOWED_HOSTS = ["*"]
+DEBUG = False  # Set to False in production
+
+ALLOWED_HOSTS = ["esp-dash.onrender.com"]  # Add your Render domain
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
